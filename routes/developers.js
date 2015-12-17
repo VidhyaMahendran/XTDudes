5182aec3bac3edb0c053ce88ce288987d4c42532
VidhyaMahendran <vidhya.mahendran@gmail.com>
2015-12-17 17:32:13 +0530

    Renaming users to developers

--- routes/developers.js
+++ routes/developers.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET developers listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'XT Dudes' });
+  res.render('developers', { title: 'XT Dudes' });
 });
 
 module.exports = router;
