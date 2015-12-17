846a4c05b5c340ec56e52cf49069da08fd7beedc
VidhyaMahendran <vidhya.mahendran@gmail.com>
2015-12-17 17:54:28 +0530

    Updated JADE spacing to tab space for easier development

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var developers = require('./routes/developers');
+var search = require('./routes/search');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/developers', developers);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'XT Dudes - Home page' });
+  res.render('index', {
+  	title: 'XT Dudes - Home page',
+  	content: 'Welcome to the XT Dudes Home page'
+  });
 });
 
 module.exports = router;
