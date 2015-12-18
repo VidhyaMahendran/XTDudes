abdecc1669f88d9803c85dbddad5f96ad0f70e56
VidhyaMahendran <vidhya.mahendran@gmail.com>
2015-12-18 15:58:35 +0530

    Adding the template for Add

---
+++ routes/add.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET developers listing. */
+router.get('/', function(req, res, next) {
+  res.render('add', { title: 'Adding new XT Dudes' });
+});
+
+module.exports = router;
